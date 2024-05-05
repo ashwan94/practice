6f3fe1a7c931f1a16eddae1366dbdea4882a3fa3
na <ckd04p@gmail.com>
2024-05-05 10:02:05 +0900

    feature 에 저장

--- branch/src/main/java/org/example/App.java
+++ branch/src/main/java/org/example/App.java
@@ -6,5 +6,7 @@ package org.example;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Feature 에 변경만 하면 반영될까?");
     }
+
 }
