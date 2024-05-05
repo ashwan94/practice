edae9c289f3c674a92d2ca1e076cbaf1b59720ce
na <ckd04p@gmail.com>
2024-05-05 10:06:24 +0900

    Revert "feature 에 저장"
    This reverts commit 6f3fe1a7c931f1a16eddae1366dbdea4882a3fa3.

--- branch/src/main/java/org/example/App.java
+++ branch/src/main/java/org/example/App.java
@@ -6,7 +6,5 @@ package org.example;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("Feature 에 변경만 하면 반영될까?");
     }
-
 }
