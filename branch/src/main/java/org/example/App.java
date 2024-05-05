34d0c52cd1d8fd6bec5c0ba7d25a7f6a29a5483e
na <ckd04p@gmail.com>
2024-05-05 10:06:57 +0900

    v1.1.2

--- branch/src/main/java/org/example/App.java
+++ branch/src/main/java/org/example/App.java
@@ -6,5 +6,16 @@ package org.example;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+<<<<<<< Updated upstream
     }
+=======
+<<<<<<< HEAD
+        System.out.println("Feature 에 변경만 하면 반영될까?");
+        System.out.println("브랜치 전환만 했는데 인텔리제이에서 코드가 변하네");
+    }
+
+=======
+    }
+>>>>>>> develop
+>>>>>>> Stashed changes
 }
